c313289e819d15b891470a4a8043a41fe05d9b9b
swaraj7 <saurav.swaraj@grofers.com>
2016-06-09 14:01:25 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,5 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
